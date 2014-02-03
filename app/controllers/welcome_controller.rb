956ce507aa9dbb969a3e082343ab3a6ef0d9dd66
chuckrea <chuckr523@mac.com>
2014-02-03 16:24:58 -0500

    texing to all users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,18 +9,21 @@ class WelcomeController < ApplicationController
 
   def create
 
+
     account_sid = 'ACfb7ac7ffa3b4d03c419ae4d98c32afd6'
     auth_token = '63c3b515f991dd71c83cf9bf47031bca'
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
 
-    
-    @client.account.messages.create(
-      :from => '+16463623947',
-      :to => params[:phone],
-      :body => "It's Mom!  You can call me, you know!"
-    )
+    users = User.all
+    users.each do |user|
+      @client.account.messages.create(
+        :from => '+16463623947',
+        :to => user.phone_number,
+        :body => "It's Mom!  You can call me, you know!"
+      )
+    end
 
     redirect_to '/'
 
