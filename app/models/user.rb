c0e7388c57bb6e0b9f0f00d11c749910388659e7
dbushy727 <d.bushkanets@gmail.com>
2014-02-03 14:10:27 -0500

    added attr_accessible to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :location, :weather, :username, :phone_number
   # attr_accessible :title, :body
 end
