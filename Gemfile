4cd0091bb9ab6faf5dc248205c11a4295ab45ab2
chuckrea <chuckr523@mac.com>
2014-02-03 15:29:42 -0500

    working on texting

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ end
 gem 'jquery-rails'
 
 gem 'twilio-ruby'
+gem 'pry'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,32 @@
 class WelcomeController < ApplicationController
 
+# put your own credentials here
+
+
   def index
+
+  end
+
+  def create
+
+    account_sid = 'ACfb7ac7ffa3b4d03c419ae4d98c32afd6'
+    auth_token = '63c3b515f991dd71c83cf9bf47031bca'
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    
+    @client.account.messages.create(
+      :from => '+16463623947',
+      :to => params[:phone],
+      :body => 'Hey there!'
+    )
+
+    redirect_to '/'
+
   end
 
 
 
-  
+
 end
